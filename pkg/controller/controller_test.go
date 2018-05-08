e26c404ceb2ab692411d730c01a4f3e0d6824acc
kahootali <kahoot.ali@gmail.com>
2018-05-08 15:37:05 +0500

    fix pod update test

--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -10,6 +11,7 @@ import (
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/util/retry"
 )
 
 var (
@@ -91,6 +93,7 @@ func TestControllerForUpdatePodShouldUpdateAndSendMessage(t *testing.T) {
 	configFilePath := "../../configs/testConfigs/CorrectConfig.yaml"
 	configuration, err := config.ReadConfig(configFilePath)
 	controller, err := NewController(clientSet, configuration.Controllers[0])
+
 	if err != nil {
 		log.Printf("Unable to create NewController error = %v", err)
 		return
@@ -101,28 +104,36 @@ func TestControllerForUpdatePodShouldUpdateAndSendMessage(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	namespace := "test"
 	podName := "testpod-withoutresources-chowkidar"
+	podClient := clientSet.CoreV1().Pods(namespace)
 	pod := podWithoutResources(namespace, podName)
-	result, err := clientSet.CoreV1().Pods(namespace).Create(pod)
+	pod, err = podClient.Create(pod)
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("Created Pod %q.\n", result.GetObjectMeta().GetName())
+	log.Printf("Created Pod %q.\n", pod.GetObjectMeta().GetName())
 	time.Sleep(5 * time.Second)
-	// pod.Spec.Containers[0].Resources = v1.ResourceRequirements{
-	// 	Limits: v1.ResourceList{
-	// 		"cpu":    resource.Quantity{},
-	// 		"memory": resource.Quantity{},
-	// 	},
-	// }
-	// log.Printf("Updated Pod %q.\n", result.GetObjectMeta().GetName())
-	// result, err = clientSet.CoreV1().Pods(namespace).Update(pod)
-	if err != nil {
-		panic(err)
+	pod.Spec.Containers[0].Resources = v1.ResourceRequirements{
+		Limits: v1.ResourceList{
+			"cpu":    resource.Quantity{},
+			"memory": resource.Quantity{},
+		},
+	}
+	log.Printf("Updating Pod %q.\n", pod.GetObjectMeta().GetName())
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		pod, err = podClient.Get(podName, metav1.GetOptions{})
+		if err != nil {
+
+		}
+		_, updateErr := podClient.Update(pod)
+		return updateErr
+	})
+	if retryErr != nil {
+		controller.clientset.CoreV1().Pods(namespace).Delete(podName, &metav1.DeleteOptions{})
+		panic(fmt.Errorf("Update failed: %v", retryErr))
 	}
 	time.Sleep(10 * time.Second)
-
+	log.Printf("Deleting Pod %q.\n", pod.GetObjectMeta().GetName())
 	controller.clientset.CoreV1().Pods(namespace).Delete(podName, &metav1.DeleteOptions{})
-
 }
 
 func podWithResources(namespace string, podName string) *v1.Pod {
